ad047f7b77509bc1eb51fd631dbd1eb56babc8c8
Rebecca Krosnick <rkros@umich.edu>
2021-11-08 15:51:29 -0500

    Edited examples

--- pages/index.js
+++ pages/index.js
@@ -5,9 +5,16 @@ import NaturalLanguage from '../components/NaturalLanguage';
 export default function Home() {
   return (
     <div>
+        {/* <NaturalLanguage text="Which MLB player had the most home runs in 2019?"/> */}
+        {/* <NaturalLanguage text="Book a flight from Detroit to Boston on November 20"/> */}
+        {/* <NaturalLanguage text="Order a chicken burrito bowl with black beans, white rice, cheese, and green salsa from Chipotle. Place the order now and for delivery."/> */}
+        <NaturalLanguage text="Schedule pick-up for a dozen chocolate ganache cupcakes from Georgetown Cupcake tomorrow"/>
+        
+        
+        
+        
+        {/*
         <NaturalLanguage text="Which MLB player had the most home runs this year?"/>
-        {/* <NaturalLanguage text="Book a flight from Detroit to Boston on October 20"/>
-        <NaturalLanguage text="Order a chicken burrito bowl with black beans, white rice, cheese, and green salsa from Chipotle. Place the order now and for delivery."/>
         <NaturalLanguage text="Order one scoop of coffee ice cream from Blank Slate"/>
         <NaturalLanguage text="Order a little cheeseburger from Five Guys with mayo, lettuce, and grilled onions"/>
         <NaturalLanguage text="What movie won Best Picture at the Oscars in 2011?"/> */}
