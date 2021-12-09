50f5eab4c09f041460cc62b7e7b6e011714305d5
Rebecca Krosnick <rkros@umich.edu>
2021-12-09 12:07:50 -0500

    Now have a participant ID field on demographic survey page

--- pages/demographicSurvey.js
+++ pages/demographicSurvey.js
@@ -1,17 +1,26 @@
 import { useRouter } from 'next/router';
+import { useState } from 'react';
 import styles from '../components/styles.module.css';
 
 export default function DemographicSurvey() {
     const router = useRouter();
     const { clientID } = router.query;
 
+    const [participantID, updateParticipantID] = useState("");
+
     function goToTask(clientID){
         
-        // Use router to navigate to next page
-        router.push({
-            pathname: '/task',
-            query: { clientID: clientID },
-        });
+        if(participantID === "" || participantID === null){
+            window.alert("Fill in participant ID");
+            updateParticipantID(null); // using null as signifier that participantID is not filled in (UI shows red error border)
+            window.scroll(0, 0); // scroll to top of page for user to see blank participant ID field
+        }else{
+            // Use router to navigate to next page
+            router.push({
+                pathname: '/task',
+                query: { clientID: clientID },
+            });
+        }
     }
 
     function iframeLoaded(scrollHeight) {
@@ -23,8 +32,27 @@ export default function DemographicSurvey() {
         }   
     }
 
+    function handleParticipantIDUpdate(e){
+        if(e.target.value === ""){
+            updateParticipantID(null); // using null as signifier that participantID is not filled in (UI shows red error border)
+        }else{
+            updateParticipantID(e.target.value);
+        }
+    }
+
     return (
         <div>
+            <div>
+                <label htmlFor="participantIDField">Participant ID: </label>
+                <input
+                    type="text"
+                    id="participantIDField"
+                    value={participantID}
+                    onChange={(e) => handleParticipantIDUpdate(e)}
+                    className={participantID === null ? styles.incompleteForm : "" }
+                >
+                </input>
+            </div>
             <div>
                 <iframe
                     src="https://docs.google.com/forms/d/e/1FAIpQLScSORDPbl3wbBTzrEeyygPMpatnIpsG33mGLQal8G_HBwtC8g/viewform?embedded=true"
