f60b7d74347be2765889557ce02947cc616336ed
Rebecca Krosnick <rkros@umich.edu>
2022-03-26 18:55:51 -0400

    Added likert scale question to 'finished' page

--- pages/finished.js
+++ pages/finished.js
@@ -4,8 +4,25 @@ export default function Finished() {
     const router = useRouter();
     const { clientID } = router.query;
     return (
-        <p>
-            Finished. If you accessed this task via Mechanical Turk, please enter <b>{clientID}</b> as the final code. 
-        </p>
+        <>
+            <p>
+                Finished. Please fill out this Google form and use <b>{clientID}</b> as the client ID.
+            </p>
+            <div>
+                <iframe
+                    src="https://docs.google.com/forms/d/e/1FAIpQLSccLdCJOBEqoOwokVe03_WlDOED8b3POZWS5SDyvEi7YEl2fQ/viewform?embedded=true"
+                    //width="640"
+                    width="100%"
+                    height="574"
+                    frameBorder="0"
+                    marginHeight="0"
+                    marginWidth="0"
+                    id="demographicSurvey"
+                    //onLoad={() => iframeLoaded(document.body.scrollHeight)}
+                >
+                    Loading…
+                </iframe>
+            </div>
+        </>
     )
 }
